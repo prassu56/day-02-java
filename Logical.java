572988e78fb3b1a162c224cac8a2f9a3f3caf6bb
prassu56 <prasuprasanna236@gmail.com>
2024-12-03 12:38:08 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Logical{
+    public static void main(String[]args){
+        System.out.println(true&&true);
+         System.out.println(false||true);
+          System.out.println(!(true));
+    }
+}
