379811758c02afe69aff9bae84e1190eca3a2198
prassu56 <prasuprasanna236@gmail.com>
2024-12-03 14:36:49 +0530

    Add files via upload

---
+++ BitwiseOr.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class BitwiseOr{
+    public static void main(String[]args){
+        int a;
+        int b;
+        System.out.println(22|144);
+        System.out.println(88|122);
+        System.out.println(11|18);
+    }
+}
