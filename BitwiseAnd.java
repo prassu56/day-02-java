48d4c6e56eca64de69a51596819f6c64813a3eeb
prassu56 <prasuprasanna236@gmail.com>
2024-12-03 14:33:14 +0530

    Add files via upload

---
+++ BitwiseAnd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class BitwiseAnd{
+    public static void main(String[]args){
+        int a;
+        int b;
+        System.out.println(10&12);
+        System.out.println(2&18);
+        System.out.println(18&15);
+         System.out.println(16&14);
+          System.out.println(12&11);
+           System.out.println(13&18);
+    }
+}
