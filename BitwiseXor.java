aa5debee32bd9c79cc666dfb987783432161d707
prassu56 <prasuprasanna236@gmail.com>
2024-12-03 15:49:11 +0530

    Add files via upload

---
+++ BitwiseXor.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class BitwiseXor{
+    public static void main(String[]args){
+        int a;
+        int b;
+        System.out.println(120^11);
+        System.out.println(122^122);
+        System.out.println(130^140);
+         System.out.println(110^110);
+          System.out.println(120^130);
+           System.out.println(5^5);
+    }
+}
