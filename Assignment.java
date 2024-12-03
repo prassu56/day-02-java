f9dc967d68639f6dcf009c09e5fe5bd6911e3684
prassu56 <prasuprasanna236@gmail.com>
2024-12-03 10:49:35 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Assignment{
+    public static void main(String[]args){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        System.out.println(a+=b);
+        System.out.println(a-=b);
+        System.out.println(a*=b);
+        System.out.println(a/=b);
+        System.out.println(a%=b);
+        System.out.println(++a + b++);
+        System.out.println(++a);
+        System.out.println(a++);
+        System.out.println(--a);
+        System.out.println(a--);
+
+    }
+
+}
