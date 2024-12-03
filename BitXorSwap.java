59b3b25e68bf48c872277b62346e05a802ca18c3
prassu56 <prasuprasanna236@gmail.com>
2024-12-03 15:20:19 +0530

    Add files via upload

---
+++ BitXorSwap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class BitXorSwap{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        String res=String.format("After swapping\na=%d\nb=%d",a,b);
+        System.out.println(res);
+    }
+}
